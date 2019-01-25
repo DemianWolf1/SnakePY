87d197b309c88e1479fbfbf19d41fd85b946d304
DemianWolf1 <Chmutova>
2019-01-25 12:05:19 +0200

    SnakePY First Commit Add: 25.01.19 HomeWork

--- bounds.py
+++ bounds.py
@@ -4,7 +4,7 @@ import pygame
 class Bounds:
     def createlist(self, width=640, height=480, size=10):
         """ in - (self)
-Creates a list of borderss Rect objects.
+Creates a list of borders Rect objects.
 out - list"""
         bounds = []
         bounds.append(pygame.Rect((0, height - size), (width, size))) # down bound
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
-        scr.fill((0,0,0))
+        scr.fill((0, 0, 0))
         for i in Bounds().createlist(width=640, height=480, size=10):
             pygame.draw.rect(scr, (100, 100, 255), i)
         pygame.display.flip()
